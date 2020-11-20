b4987c03a7297e63721ee03c28e3da88e86ac201
sadatakhtar <sadat0akhtar@gmail.com>
2020-11-20 10:59:33 +0000

    renamed myPdPost to pdPostBody and removed empty space

--- src/routes/students/controllers.js
+++ src/routes/students/controllers.js
@@ -17,8 +17,7 @@ export const getStudentProfile = async (req, res) => {
     filteredProfile ?  res
     .status(200)
     .send(filteredProfile) : res.json({success: false});
-  
-    
+
   } catch (err) {
     console.log(err);
     return res.status(400).send("Could not get students");
@@ -27,13 +26,8 @@ export const getStudentProfile = async (req, res) => {
 
 export const postPdSkills = async (req, res) => {
   try {
-    let myPdPost = req.body;
-    !myPdPost.profile ? res.send('invalid data schema, need to add profile within POST!') : (pdSkillsPostData.push(myPdPost), res.status(200).json(myPdPost));
-    
-    
-   
-  
-    
+    let pdPostBody = req.body;
+    !pdPostBody.profile ? res.send('invalid data schema, need to add profile within POST!') : (pdSkillsPostData.push(pdPostBody), res.status(200).json(myPdPost));
   } catch (err) {
     console.log(err);
     return res.status(400).send("Could not get students Pd skills data");
