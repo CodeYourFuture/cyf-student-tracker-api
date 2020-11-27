eaf6207e60ea90d3b1e06745899e93315928ea95
sadatakhtar <sadat0akhtar@gmail.com>
2020-11-27 12:12:14 +0000

    added class information to /auth/login

--- src/routes/auth/controllers.js
+++ src/routes/auth/controllers.js
@@ -7,6 +7,11 @@ export const login = async (req, res) => {
     !authenticated ? res.json({success: false}): res.json({
       success: true,
       user: postBody.userName,
+      class: {
+        class1: "West Midlandss class 1",
+        class2: "West Midlandss class 2",
+        class3: "West Midlandss class 3"
+      }
     });
     
     
